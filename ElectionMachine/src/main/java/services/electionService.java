3e80e203a3fb2c7a5dc11b2ce3a32a0176c20548
EeviVinnari <eevi.vinnari@student.hamk.fi>
2022-05-12 20:21:04 +0300

    .

--- ElectionMachine/src/main/java/services/electionService.java
+++ ElectionMachine/src/main/java/services/electionService.java
@@ -1,4 +1,4 @@
-package services;
+package services; //package of this class
 
 import java.io.IOException;
 import java.util.List;
@@ -25,9 +25,15 @@ import javax.ws.rs.core.MediaType;
 
 import app.model.Kysymykset;
 
+<<<<<<< HEAD
 @Path("/electionservice")
 public class electionService {
 	EntityManagerFactory emf=Persistence.createEntityManagerFactory("vaalikone");
+=======
+@Path("/electionservice") //path to this service's Root Resource class 
+public class electionService { //signature of the class
+	EntityManagerFactory emf=Persistence.createEntityManagerFactory("vaalikone"); //create connection with database. persistence.xml file for settings
+>>>>>>> 1090a25c5b7e2b1a44b121319939d968f44ffd0b
 	@Context
 	HttpServletRequest request;
 	
@@ -35,6 +41,7 @@ public class electionService {
 	HttpServletResponse response;
 	
 	
+<<<<<<< HEAD
 	@GET
 	@Path("/readquestion")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -47,11 +54,26 @@ public class electionService {
 		RequestDispatcher rd=request.getRequestDispatcher("/jsp/showquestions.jsp");
 		request.setAttribute("questionlist", list);
 		try {
+=======
+	@GET //the method accepts only GET-type requests
+	@Path("/readquestion") //path to this method
+	@Produces(MediaType.APPLICATION_JSON) //defines what media type method produces
+	@Consumes(MediaType.APPLICATION_JSON) //defines what media type method accepts
+	public void readKysymykset() {
+		EntityManager em=emf.createEntityManager(); //creates EntityManager which manages entities given in persistence.xml
+		em.getTransaction().begin(); //starts the transaction (every transaction must begin and end)
+		List<Kysymykset> list=em.createQuery("select xyx from Kysymykset xyx").getResultList();	//reads all the data from a database table kysymykset and returns a list of objects. the 'xyx's could be anything but they need to be exactly the same	
+		em.getTransaction().commit(); //closes the transaction
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/showquestions.jsp"); //creates a requestDispatcher rd, and shows where it should be forwarded. requestDispatcher allows forwarding a request/response to a resource, for example JSP.
+		request.setAttribute("questionlist", list); //adds attribute to request to take to showquestions.jsp
+		try { //tries to forward the request with the attribute and catches an exception if it doesn't go through
+>>>>>>> 1090a25c5b7e2b1a44b121319939d968f44ffd0b
 			rd.forward(request, response);
 		} catch (ServletException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+<<<<<<< HEAD
 	}	
 	@POST
 	@Path("/addquestion")
@@ -59,9 +81,19 @@ public class electionService {
 	@Consumes("application/x-www-form-urlencoded")
 	public void addKysymykset(@FormParam("kysymys") String kysymys) {  
 		Kysymykset k=new Kysymykset(kysymys);
+=======
+	}
+	
+	@POST //the method accepts only POST-type requests
+	@Path("/addquestion")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes("application/x-www-form-urlencoded")
+	public void addKysymykset(@FormParam("kysymys") String kysymys) {  //we are using FormParam because we get the parameters from an HTML form
+		Kysymykset k=new Kysymykset(kysymys); //creating a new Kysymykset object
+>>>>>>> 1090a25c5b7e2b1a44b121319939d968f44ffd0b
 		EntityManager em=emf.createEntityManager();
 		em.getTransaction().begin();
-		em.persist(k);//The actual insertion line
+		em.persist(k); //Inserting kysymykset object into database
 		em.getTransaction().commit();
 		//Calling the method readKysymykset() of this service
 		readKysymykset();		
@@ -72,10 +104,10 @@ public class electionService {
 	@Consumes("application/x-www-form-urlencoded")
 	public void updateKysymykset(@FormParam("kysymys") String kysymys, @FormParam("kysymys_id") int kysymys_id) {  
 		Kysymykset k=new Kysymykset(kysymys);
-		k.setKysymys_id(kysymys_id);
+		k.setKysymys_id(kysymys_id); //object kysymykset gets kysymys_id as parameter
 		EntityManager em=emf.createEntityManager();
 		em.getTransaction().begin();
-		em.merge(k);//The actual insertion line
+		em.merge(k); //Updates kysymykset in database
 		em.getTransaction().commit();
 		//Calling the method readKysymykset() of this service
 		readKysymykset();		
@@ -97,17 +129,25 @@ public class electionService {
 	}	
 
 	@GET
-    @Path("/delete/{kysymys_id}")
+    @Path("/delete/{kysymys_id}") //setting kysymys_id as a parameter into URL
     @Produces(MediaType.APPLICATION_JSON)
+<<<<<<< HEAD
     public void deleteAnswers(@PathParam("kysymys_id") int id) { 
         EntityManager em=emf.createEntityManager();
         em.getTransaction().begin();
         Kysymykset k=em.find(Kysymykset.class, id);
         if (k!=null) {
+=======
+    public void deleteAnswers(@PathParam("kysymys_id") int id) { // when using PathParam we get the parameters from URL (kysymys_id)
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        Kysymykset k=em.find(Kysymykset.class, id); //searches object id (kysymys_id) from database
+        if (k!=null) { //if k (kysymykset) is not null, meaning that there is information, the chosen line is being deleted
+>>>>>>> 1090a25c5b7e2b1a44b121319939d968f44ffd0b
             em.remove(k);//The actual insertion line
         }
         em.getTransaction().commit();
-        //Calling the method readAnswers() of this service
+        //Calling the method readKysymykset() of this service
         readKysymykset();
         }
 	@GET
@@ -124,7 +164,7 @@ public class electionService {
 		try {
 			rd.forward(request, response);
 		} catch (ServletException | IOException e) {
-			// TODO Auto-generated catch block
+			// TODO Auto-generated catch block jee
 			e.printStackTrace();
 		} 
 	}	
